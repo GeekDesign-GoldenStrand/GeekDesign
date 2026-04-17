036f749fbf3b203d49de78b6aa17eb4c7e04317d
CsVazquezz <149129755+CsVazquezz@users.noreply.github.com>
2026-04-17 11:56:43 -0600

    [FIX] fix CI convert jest config to JS, add prisma generate to all jobs

---
+++ portal-geek/jest.config.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({ dir: "./" });
+
+/** @type {import('jest').Config} */
+const config = {
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  collectCoverageFrom: [
+    "app/**/*.{ts,tsx}",
+    "lib/**/*.{ts,tsx}",
+    "components/**/*.{ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+  ],
+};
+
+module.exports = createJestConfig(config);
