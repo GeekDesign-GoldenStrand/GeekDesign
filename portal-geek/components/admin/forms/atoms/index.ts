7503cb03f4d141bbb01ea1861100f4f3ba599547
Ricardo Calzada <a01707647@tec.mx>
2026-04-25 15:14:48 -0600

    [aA

---
+++ portal-geek/components/admin/forms/atoms/index.ts
@@ -0,0 +1,5 @@
+export { input } from "./input";
+export { textarea } from "./Textarea";
+export { Select } from "./select";
+export { Checkbox } from "./checkbox";
+export { Button } from "./button";
