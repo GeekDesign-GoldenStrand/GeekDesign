8681c33daad9ea07bfd6de8132be132c1918ee3a
CsVazquezz <149129755+CsVazquezz@users.noreply.github.com>
2026-04-26 18:12:40 -0600

    [FIX] Missing accent que in CTABanner

--- portal-geek/components/storefront/molecules/CTABanner.tsx
+++ portal-geek/components/storefront/molecules/CTABanner.tsx
@@ -4,7 +4,7 @@ export function CTABanner() {
   return (
     <section className="bg-[#fff8f9] flex flex-col items-center justify-center py-10 md:py-[60px] gap-5 md:gap-[26px]">
       <h2 className="font-bold text-[22px] md:text-[30px] text-[#1e1e1e] text-center leading-normal px-4">
-        ¿No sabes que quieres?
+        ¿No sabes qué quieres?
       </h2>
       <Link
         href="/cotizacion"
