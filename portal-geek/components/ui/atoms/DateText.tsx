168dba1171026c5d4d27fd2021c58ece2e0ccc30
XimePerezEscalante <A01751827@tec.mx>
2026-05-02 23:30:11 -0600

    [ADD] date formatter component file

---
+++ portal-geek/components/ui/atoms/DateText.tsx
@@ -0,0 +1,24 @@
+export function DateText(dateString: string): string {
+  const date = new Date(dateString);
+
+  const months = [
+    "ENE",
+    "FEB",
+    "MAR",
+    "ABR",
+    "MAY",
+    "JUN",
+    "JUL",
+    "AGO",
+    "SEP",
+    "OCT",
+    "NOV",
+    "DIC",
+  ];
+
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  return `${day} ${month} ${year}`;
+}
