985407f3f21e8b6989c459bc257b45cf1e722a45
XimePerezEscalante <A01751827@tec.mx>
2026-05-05 10:50:24 -0600

    [REFACTOR] delete old cotizacion component folder

--- portal-geek/components/cotizaciones/molecules/NextPageButton.tsx
+++
@@ -1,9 +0,0 @@
-import { ArrowRight } from "@/components/cotizaciones/atoms/ArrowRight";
-
-export function NextPageButton() {
-    return (
-        <button className="flex items-center gap-2 text-sm font-medium text-[#1e1e1e]">
-            <ArrowRight />
-        </button>
-    );
-}
--- portal-geek/components/cotizaciones/molecules/PreviousPageButton.tsx
+++
@@ -1,9 +0,0 @@
-import { ArrowLeft } from "@/components/cotizaciones/atoms/ArrowLeft";
-
-export function PreviousPageButton() {
-    return (
-        <button className="flex items-center gap-2 text-sm font-medium">
-            <ArrowLeft />
-        </button>
-    );
-}
