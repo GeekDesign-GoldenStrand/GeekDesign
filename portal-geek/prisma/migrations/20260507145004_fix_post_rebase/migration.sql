a3adf98dfef7bc20f7d031e3a2cae88d750814b4
Ricardo Calzada <a01707647@tec.mx>
2026-05-07 20:33:42 -0600

    [FIX] add fecha_modificacion and sucursal relation to Servicios

---
+++ portal-geek/prisma/migrations/20260507145004_fix_post_rebase/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `nombre_proveedor` on the `INSTALADORES` table. All the data in the column will be lost.
+  - Added the required column `nombre_instalador` to the `INSTALADORES` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "INSTALADORES" DROP COLUMN "nombre_proveedor",
+ADD COLUMN     "nombre_instalador" VARCHAR(100) NOT NULL;
