e6f9528b8e786fd3694e9cd0f7a927119c3d00ed
XimePerezEscalante <A01751827@tec.mx>
2026-05-20 18:57:46 -0600

    [FIX] SideBarLogo position

--- portal-geek/components/admin/sidebar/Sidebar.tsx
+++ portal-geek/components/admin/sidebar/Sidebar.tsx
@@ -6,7 +6,7 @@ import { SidebarNav } from "./molecules/SidebarNav";
 
 export function Sidebar({ role }: { role: UserRole }) {
   return (
-    <aside className="fixed left-0 top-0 h-screen w-16 md:w-[102px] bg-white shadow-[0px_4px_10px_rgba(0,0,0,0.08)] flex flex-col items-center py-6 z-50 border-r border-[#F0F0F0]">
+    <aside className="fixed left-0 top-0 h-screen w-16 md:w-[102px] bg-white shadow-[0px_4px_10px_rgba(0,0,0,0.08)] flex flex-col items-center py-2 z-50 border-r border-[#F0F0F0]">
       <SidebarLogo />
       <div className="flex-1 w-full overflow-y-auto">
         <SidebarNav role={role} />
